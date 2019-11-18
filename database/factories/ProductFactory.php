0d358af86e79fa98c32ec028554df26dc1d6b113
proyectosjosegouv <proyectosjosegouv@gmail.com>
2019-11-17 23:06:41 -0400

    Migracion y seeder listo

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence(2),
+        'short' =>$faker->text(140),
+        'body' => $faker->text(900),
+    ];
+});
